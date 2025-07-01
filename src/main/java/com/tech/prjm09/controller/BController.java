ac26a0d9fa4a7284f147862820ace87481f5c672
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-01 17:03:15 +0900

    feat: 게시글 수정기능 modify를 BModifyService로 분리

--- src/main/java/com/tech/prjm09/controller/BController.java
+++ src/main/java/com/tech/prjm09/controller/BController.java
@@ -21,6 +21,7 @@ import com.tech.prjm09.dto.ReBrdimgDto;
 import com.tech.prjm09.service.BContentViewService;
 import com.tech.prjm09.service.BDownloadService;
 import com.tech.prjm09.service.BListService;
+import com.tech.prjm09.service.BModifyService;
 import com.tech.prjm09.service.BModifyViewService;
 import com.tech.prjm09.service.BServiceInter;
 import com.tech.prjm09.service.BWriteService;
@@ -115,16 +116,10 @@ public class BController {
 	@PostMapping("modify")
 	public String modify(HttpServletRequest request, Model model) {
 		System.out.println("modify() ctr");		
-//		model.addAttribute("request", request);
-//		command = new BModifyCommand();
-//		command.execute(model);
 		
-		String bid = request.getParameter("bid");
-		String bname = request.getParameter("bname");
-		String btitle = request.getParameter("btitle");
-		String bcontent = request.getParameter("bcontent");
-		
-		iDao.modify(bid, bname, btitle, bcontent);
+		model.addAttribute("request", request);
+		bServiceInter = new BModifyService(iDao);
+		bServiceInter.execute(model);
 		
 		return "redirect:list";
 	}
